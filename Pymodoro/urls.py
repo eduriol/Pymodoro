e3e21575aa87cdebbe59e7931e6775285ca80d66
eduriol <eduriol@gmail.com>
2013-10-01 20:10:44 +0200

    Start action set up.

--- Pymodoro/urls.py
+++ Pymodoro/urls.py
@@ -4,5 +4,6 @@ from Pymodoro import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^start/$', views.start, name='start'),
     url(r'^(?P<pomodoro_id>\d+)/$', views.detail, name='detail'),
 )
--- Pymodoro/views.py
+++ Pymodoro/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 from Pymodoro.models import Pomodoro
 
 def index(request):
     latest_pomodoro_list = Pomodoro.objects.order_by('-end_time')[:5]
-    output = ', '.join([p.__unicode__() for p in latest_pomodoro_list])
-    return HttpResponse(output)
+    context = {'latest_pomodoro_list': latest_pomodoro_list}
+    return render(request, 'Pymodoro/index.html', context)
 
 def detail(request, pomodoro_id):
-    return HttpResponse("You're looking at pomodoro %s." % pomodoro_id)
+    pomodoro = get_object_or_404(Pomodoro, pk=pomodoro_id)
+    return render(request, 'Pymodoro/detail.html', {'pomodoro': pomodoro})
+
+def start(request):
+    return HttpResponseRedirect(reverse('Pymodoro:index'))
