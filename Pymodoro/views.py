f0892725b678db96008a436447c6c7d3605382ea
eduriol <eduriol@gmail.com>
2013-10-01 23:02:26 +0200

    Dummy creation of pomodoros.

--- Pymodoro/views.py
+++ Pymodoro/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 from Pymodoro.models import Pomodoro
 
@@ -13,5 +15,8 @@ def detail(request, pomodoro_id):
     pomodoro = get_object_or_404(Pomodoro, pk=pomodoro_id)
     return render(request, 'Pymodoro/detail.html', {'pomodoro': pomodoro})
 
-def start(request):
+def start(request, tag):
+    if (request.user.is_authenticated()):
+        new_pomodoro = Pomodoro(user=request.user, end_time=timezone.now(), tag=tag)
+        new_pomodoro.save()
     return HttpResponseRedirect(reverse('Pymodoro:index'))
