ace13aaeb8f3e13c3cc483cfffd6157004ebd736
eduriol <eduriol@gmail.com>
2013-10-02 17:27:56 +0200

    Pomodoro saved after a harcoded time.

--- Pymodoro/views.py
+++ Pymodoro/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils import timezone
-from django.contrib.auth.models import User
+import time
 
 from Pymodoro.models import Pomodoro
 
 def index(request):
-    latest_pomodoro_list = Pomodoro.objects.order_by('-end_time')[:5]
+    latest_pomodoro_list = Pomodoro.objects.filter(is_from_today=True)
     context = {'latest_pomodoro_list': latest_pomodoro_list}
     return render(request, 'Pymodoro/index.html', context)
 
@@ -15,8 +15,9 @@ def detail(request, pomodoro_id):
     pomodoro = get_object_or_404(Pomodoro, pk=pomodoro_id)
     return render(request, 'Pymodoro/detail.html', {'pomodoro': pomodoro})
 
-def start(request, tag):
+def start(request):
     if (request.user.is_authenticated()):
-        new_pomodoro = Pomodoro(user=request.user, end_time=timezone.now(), tag=tag)
+        time.sleep(1*1) # minutes * seconds/minute
+        new_pomodoro = Pomodoro(user=request.user, end_time=timezone.now(), tag=request.POST['tag'])
         new_pomodoro.save()
     return HttpResponseRedirect(reverse('Pymodoro:index'))
