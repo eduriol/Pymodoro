a915573c2bdeef59d9ce4f2a116e19414bab7fd4
eduriol <eduriol@gmail.com>
2013-10-03 17:46:32 +0200

    Showing only pomodoros from authenticated user.

--- Pymodoro/models.py
+++ Pymodoro/models.py
@@ -10,7 +10,7 @@ class Pomodoro(models.Model):
     end_time = models.DateTimeField("end time")
 
     def __unicode__(self):
-        return '%s, from %s to %s in %s' % (self.user.username, self.init_time(), self.end_time, self.tag)
+        return 'from %s to %s in %s' % (self.user.username, self.init_time(), self.end_time, self.tag)
 
     def init_time(self):
         return self.end_time - datetime.timedelta(minutes=25)
@@ -22,9 +22,9 @@ class Pomodoro(models.Model):
     is_from_today.short_description = 'is from today?'
 
 class PomodoroManager(models.Manager):
-    def are_from_today(self):
+    def are_from_today(self, user):
         result_list = []
         for p in Pomodoro.objects.all():
-            if p.is_from_today():
+            if p.is_from_today() and p.user == user:
                 result_list.append(p)
         return sorted(result_list, key=attrgetter('end_time'), reverse=True)
